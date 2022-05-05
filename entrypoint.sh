bc4bfdded72c83f65e5cdbb719322ee38ad30cdf
albertdev.tw <45681803+albertdevtw@users.noreply.github.com>
2022-05-05 14:12:51 +0800

    fix: set JENKINS_USER

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,16 +8,7 @@ fi
 
 uid=$(stat -c %u $JENKINS_HOME)
 gid=$(stat -c %g $JENKINS_HOME)
-u=$(getent passwd $uid | cut -d: -f1)
-
-if [ -z "$u" ]; then
-    usermod -u $uid $JENKINS_USER
-else
-    JENKINS_USER=$u
-fi
-
-if [ -z "$(getent group $gid)" ]; then
-    groupmod -g $gid $JENKINS_USER
-fi
+[ -z "$(getent passwd $uid)" ] && usermod -u $uid $JENKINS_USER || JENKINS_USER=$(id -un $uid)
+[ -z "$(getent group $gid)" ] && groupmod -g $gid $JENKINS_USER
 
 exec gosu $JENKINS_USER tini -- /usr/local/bin/jenkins.sh "$@"
